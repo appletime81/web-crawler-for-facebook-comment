869765474a4662de81f8cb71ef6339978dc9da02
appletime81 <itrimsl8696@gmail.com>
2021-10-21 09:14:27 +0800

    Update

--- test_抓取單篇貼文.py
+++ test_抓取單篇貼文.py
@@ -15,15 +15,15 @@ prefs = {
 options.add_experimental_option('prefs', prefs)
 options.add_argument('disable-infobars')
 options.add_argument('--window-size=1920,1080')
-driver = webdriver.Chrome('./chromedriver.exe', options=options)
+driver = webdriver.Chrome('./chromedriver', options=options)
 
 # global variables
 comment_type_list = ['最相關留言', '最新', '所有留言']
 
 
-
 def browser_action():
     # open facebook
+    # driver.get('https://www.facebook.com/groups/999385510116409/posts/4039427482778848')
     driver.get('https://www.facebook.com/groups/1035885209777446/posts/5029962087036385/')
 
     # input email
@@ -38,11 +38,70 @@ def browser_action():
     driver.find_element_by_xpath('//*[@id="login_form"]/div[2]/div[3]/div/div/div[1]/div/span/span').click()
 
     # 把所有留言點擊出來
-    time.sleep(10)
+    time.sleep(5)
     all_comment_btn = driver.find_elements_by_css_selector("span.d2edcug0.hpfvmrgz.qv66sw1b")
-    print(all_comment_btn)
-    for i in all_comment_btn:
-        print(i.text)
+    for btn in all_comment_btn:
+        try:
+            if btn.text in comment_type_list:
+                btn.click()
+                time.sleep(1)
+                all_comment_btn = driver.find_elements_by_css_selector("span.d2edcug0.hpfvmrgz.qv66sw1b")
+                for btn in all_comment_btn:
+                    try:
+                        if btn.text == comment_type_list[2]:
+                            btn.click()
+                            time.sleep(3)
+                            ctrl = 1
+                            while ctrl:
+                                show_all_comment_btn = driver.find_elements_by_css_selector("span.d2edcug0.hpfvmrgz.qv66sw1b")
+                                ctrl = detect_see_more_comment(show_all_comment_btn)
+                                # for i in range(10):
+                                #     driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+                                # time.sleep(3)
+                    except Exception:
+                        pass
+        except Exception:
+            pass
+
+
+def detect_see_more_comment(show_all_comment_btn):
+    count = 0
+    for btn in show_all_comment_btn:
+        try:
+            if (
+                '顯示先前' in btn.text or '檢視另' in btn.text or '查看其他' in btn.text or '則回覆' in btn.text
+            ):
+                print('--------------------')
+                print(btn.text)
+                time.sleep(1)
+                try:
+                    btn.click()
+                    print('已點擊')
+                except Exception as e:
+                    try:
+                        driver.execute_script("arguments[0].click();", btn)
+                        print('已點擊')
+                    except Exception as e:
+                        print(e)
+
+                time.sleep(0.5)
+                count += 1
+        except:
+            pass
+    return count
+
+
+def get_users():
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    tags = soup.select('a')
+    users = list()
+    for tag in tags:
+        try:
+            if '/user/' in tag.get('href'):
+                users.append(tag.get('href'))
+        except Exception:
+            pass
+    pprint(users)
 
 
 # def get_posts():
@@ -60,4 +119,8 @@ def browser_action():
 
 
 if __name__ == '__main__':
+    import time
+    start = time.time()
     browser_action()
+    get_users()
+    print(f'總執行時間: {time.time() - start}秒')
