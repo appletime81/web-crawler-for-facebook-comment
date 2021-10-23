de706b081b8bd29ee369571903c13a03712f948d
appletime81 <itrimsl8696@gmail.com>
2021-10-23 20:38:39 +0800

    Update

--- get_latest_post.py
+++ get_latest_post.py
@@ -18,14 +18,36 @@ def browser_action():
     password.send_keys('816357492~Nanonanonano')
 
     driver.find_element_by_xpath('//*[@id="login_form"]/div[2]/div[3]/div/div/div[1]/div/span/span').click()
-
     time.sleep(1)
+
+    # ------選取貼文顯示方式------
+    post_present_method_btns = driver.find_elements_by_css_selector('span.a8c37x1j.ni8dbmo4.stjgntxs.l9j0dhe7')
+    for btn in post_present_method_btns:
+        try:
+            if btn.text in ['熱門貼文', '最新動態', '最新貼文', '最相關貼文']:
+                btn.click()
+                break
+        except Exception:
+            pass
+    time.sleep(1)
+
+    post_present_method_btns = driver.find_elements_by_css_selector('span.d2edcug0.hpfvmrgz.qv66sw1b')
+    for btn in post_present_method_btns:
+        try:
+            if btn.text == '最新動態':
+                btn.click()
+                break
+        except Exception:
+            pass
+    time.sleep(1)
+    # -------------------------
+
     for i in range(3):
         driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
         time.sleep(1)
 
 
-def get_posts():
+def get_posts(prefix_url):
     soup = BeautifulSoup(driver.page_source, "html.parser")
     tags = soup.select('a')
     post_ids = list()
@@ -45,3 +67,9 @@ def get_posts():
 
 def get_recent_posts():
     pass
+
+
+if __name__ == '__main__':
+    browser_action()
+    post_urls = get_posts(prefix_url=None)
+    pprint(post_urls)
